8c432c83f7c0bdfa5e07501c8a0b4602505aac4d
bilal <bilalshk62@gmail.com>
2021-11-14 00:38:09 +0500

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,45 @@
+import "./about.css";
+
+
+const About = () => {
+    return (
+        <div>
+             <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Developer</div>
+                        <p>I will Provide a Frontend Services of All Web Application</p>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Ecommerce Website</div>
+                        <p>I will Provide a Ecommerce Website Services.Experience of developing E-Commerce Projects.</p>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Graphic Design</div>
+                        <p>I am Provide a  Services of Graphic Design</p>
+                    </div>
+                    
+                </div>
+                  
+                  
+                </div>
+               
+            </div>
+       
+    </section>
+        </div>
+    )
+}
+export default About;
---
+++ src/components/skill/Skill.jsx
@@ -0,0 +1,67 @@
+import "./skill.css";
+
+
+const Skill = () => {
+    return (
+        <div>
+             <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p> i am a professional web developer with an experience of more than 2 years and strong expertise in HTML, CSS, CSS, Bootstrap, JavaScript, React, Redux,Mongodb and GitHub. 
+Worked on Small Project to Large Scale Websites. Experience of developing E-Commerce Projects. Good Project Organizing and Management Skills. I will assure you the best quality work with 24/7 services, unlimited revisions, and amazing seasonal offers. For any query be easy to contact me</p>
+                    
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JAVASCRIPT</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>BOOTSTRAP</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>REACT</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MONGODB</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        </div>
+    )
+}
+export default Skill;
